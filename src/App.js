9709cb50fe396e5394ffbdd9cccb50189de7bc78
Hugo Ceriani <100979006+GoldFingerTold@users.noreply.github.com>
2023-08-19 18:01:14 -0300

    testimonio finalizado

--- src/App.js
+++ src/App.js
@@ -6,7 +6,33 @@ function App() {
     <div className='App'>
       <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros alumnos sobre FreeCodeCamp</h1>
-        <Testimonio />
+        <Testimonio 
+          nombre='Shawn Wang'
+          pais='Singapur'
+          imagen='shawn'
+          cargo='Ingeniero de Software'
+          empresa='Amazon'
+          testimonio='sadfafafsadfadfgsgsgsdfgsfdgsdfgdsgsdgdsg
+          sdgsdgsdgsdfgsgsdfgdsfgdsfgsdfgsdfgsdfgsdfgs
+          dfgdsfdsfgsdgsfsgsdfgdfgsdgfsdfg'/>  
+           <Testimonio 
+          nombre='Sarah Chima'
+          pais='Nigeria'
+          imagen='sarah'
+          cargo='Ingeniera de Software'
+          empresa='ChatDesk'
+          testimonio='sadfafafsadfadfgsgsgsdfgsfdgsdfgdsgsdgdsg
+          sdgsdgsdgsdfgsgsdfgdsfgdsfgsdfgsdfgsdfgsdfgs
+          dfgdsfdsfgsdgsfsgsdfgdfgsdgfsdfg'/>   
+          <Testimonio 
+          nombre='Ema Bostian'
+          pais='Suecia'
+          imagen='emma'
+          cargo='Ingeniera de Software'
+          empresa='Spotify'
+          testimonio='sadfafafsadfadfgsgsgsdfgsfdgsdfgdsgsdgdsg
+          sdgsdgsdgsdfgsgsdfgdsfgdsfgsdfgsdfgsdfgsdfgs
+          dfgdsfdsfgsdgsfsgsdfgdfgsdgfsdfg'/>   
       </div>
     </div>
   );
