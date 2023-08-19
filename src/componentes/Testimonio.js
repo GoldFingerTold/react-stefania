ac5b0d2a4adb234397d9a64eb4a788e667d412bd
Hugo Ceriani <100979006+GoldFingerTold@users.noreply.github.com>
2023-08-19 14:26:55 -0300

    style testimonios

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../style/Testimonio.css';
 
-function Testimonio() {
+function Testimonio(props) {
 	return(
 		<div className='contenedor-testimonio'>
 			<img 
@@ -11,7 +11,7 @@ function Testimonio() {
 			<div className='contenedor-texto-testimonio'>
 				<p className='nombre-testimonio'>Emma Bostian en Suecia</p>
 				<p className='cargo-testimonio'>Ingeniera de software en Spotify</p>
-				<p className='texto-testimonio'>"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
+				<p className='texto-testimonio'>"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify".</p>
 			</div>
 		</div>
 	);
